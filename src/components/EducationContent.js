4ab2d66a176ec0a7877b97895d08b481e164977c
lizeyang1995 <424011527@qq.com>
2020-09-22 19:44:36 +0800

    [lizeyang] faet: replace div with section

--- src/components/EducationContent.js
+++ src/components/EducationContent.js
@@ -3,32 +3,32 @@ import React from 'react';
 const EducationContent = () => (
   <ul>
     <li>
-      <div className="year">1990</div>
-      <div className="introduction">
+      <section className="year">1990</section>
+      <section className="introduction">
         <p>I was born in Katowice</p>
         <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-      </div>
+      </section>
     </li>
     <li>
-      <div className="year">2005</div>
-      <div className="introduction">
+      <section className="year">2005</section>
+      <section className="introduction">
         <p>Secondary school specializing in artistic</p>
         <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-      </div>
+      </section>
     </li>
     <li>
-      <div className="year">2009</div>
-      <div className="introduction">
+      <section className="year">2009</section>
+      <section className="introduction">
         <p>First level graduation in Grapgic Design</p>
         <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-      </div>
+      </section>
     </li>
     <li>
-      <div className="year">2012</div>
-      <div className="introduction">
+      <section className="year">2012</section>
+      <section className="introduction">
         <p>Second level graduation in Graphic Desigin</p>
         <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-      </div>
+      </section>
     </li>
   </ul>
 );
