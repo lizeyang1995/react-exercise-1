1f55ff37657c882e9628b919c7928442afb3a929
lizeyang1995 <424011527@qq.com>
2020-09-22 17:01:52 +0800

    [lizeyang] faet: add Title components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.scss';
 import Image from './components/Image';
 import Hello from './components/Hello';
+import Title from './components/Title';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <main className="app">
         <Image />
         <Hello />
+        <Title />
       </main>
     );
   }
---
+++ src/components/Title.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Title = () => (
+  <h2 className="title">MY NAME IS KAMIL 24YO AND THIS IS MY RESUME/CV</h2>
+);
+export default Title;
