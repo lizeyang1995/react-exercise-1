e43542cbeb9cb901dc5ae298d302c1a3d98f4211
lizeyang1995 <424011527@qq.com>
2020-09-22 19:30:49 +0800

    [lizeyang] faet: modify EducationContent components

--- src/components/EducationContent.js
+++ src/components/EducationContent.js
@@ -3,28 +3,32 @@ import React from 'react';
 const EducationContent = () => (
   <ul>
     <li>
-      <p>I was born in Katowice</p>
-      <p>
-        Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-      </p>
+      <div className="year">1990</div>
+      <div className="introduction">
+        <p>I was born in Katowice</p>
+        <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+      </div>
     </li>
     <li>
-      <p>I was born in Katowice</p>
-      <p>
-        Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-      </p>
+      <div className="year">2005</div>
+      <div className="introduction">
+        <p>Secondary school specializing in artistic</p>
+        <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+      </div>
     </li>
     <li>
-      <p>I was born in Katowice</p>
-      <p>
-        Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-      </p>
+      <div className="year">2009</div>
+      <div className="introduction">
+        <p>First level graduation in Grapgic Design</p>
+        <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+      </div>
     </li>
     <li>
-      <p>I was born in Katowice</p>
-      <p>
-        Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-      </p>
+      <div className="year">2012</div>
+      <div className="introduction">
+        <p>Second level graduation in Graphic Desigin</p>
+        <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+      </div>
     </li>
   </ul>
 );
