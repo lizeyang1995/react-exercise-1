808362608156dd1c5871fc0009cdbc14d0e10f84
lizeyang1995 <424011527@qq.com>
2020-09-22 14:54:30 +0800

    [lizeyang] faet: modify image style and add h1 style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Image from './components/Image';
+import Hello from './components/Hello';
 
 class App extends Component {
   render() {
     return (
       <main className="app">
         <Image />
+        <Hello />
       </main>
     );
   }
---
+++ src/components/Hello.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Hello = () => <h1>HELLO,</h1>;
+export default Hello;
